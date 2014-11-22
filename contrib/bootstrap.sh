78f437cb705740446ec773514a3924d480ef2e64
Alexandre Bique <bique.alexandre@gmail.com>
2014-11-22 12:56:26 +0100

    Update qt build config

--- contrib/bootstrap.sh
+++ contrib/bootstrap.sh
@@ -5,18 +5,20 @@ rm -rf qt
 tar xf qt-everywhere-opensource-src-5.3.2.tar.xz
 mv qt-everywhere-opensource-src-5.3.2 qt
 mkdir qt-build
-cd qt-build && ../qt/configure -release \
-                               -opensource \
-                               -c++11 \
-                               -static \
-                               -process \
-                               -largefile \
-                               -no-glib \
-                               -no-pulseaudio \
-                               -no-alsa \
-                               -no-cups \
-                               -no-nis \
-                               -no-dbus \
-                               -no-directfb \
-                               -no-kms \
-                               -no-linuxfb
+cd qt-build
+../qt/configure -release \
+                -opensource \
+                -c++11 \
+                -static \
+                -process \
+                -largefile \
+                -no-glib \
+                -no-pulseaudio \
+                -no-alsa \
+                -no-cups \
+                -no-nis \
+                -no-dbus \
+                -no-directfb \
+                -no-kms \
+                -no-linuxfb \
+		-no-sql-{mysql,odbc,sqlite,pgsql}
