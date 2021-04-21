ef785b6dd73f44521ae8e1a0e4044bf4e3124f89
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-04-21 21:59:22 +0200

    Add a stream mechanism

---
+++ include/clap/ext/stream.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct clap_istream {
+   /* returns the number of bytes read.
+    * 0 for end of file.
+    * -1 on error. */
+   int64_t (*read)(struct clap_istream *stream, void *buffer, uint64_t size);
+};
+
+struct clap_ostream {
+   /* returns the number of bytes written.
+    * -1 on error. */
+   int64_t (*write)(struct clap_istream *stream, const void *buffer, uint64_t size);
+};
+
+#ifdef __cplusplus
+}
+#endif
