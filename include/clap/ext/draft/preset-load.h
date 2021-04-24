e4e3be63f97d249ab1251fdc8341e90e1cdfa6a3
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-04-25 00:11:06 +0200

    Move the preset loading to its own extension

---
+++ include/clap/ext/draft/preset-load.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../clap.h"
+
+#define CLAP_EXT_PRESET_LOAD "clap/draft/preset-load"
+
+struct clap_plugin_preset_load {
+   // Loads a preset in the plugin native preset file format from a path.
+   // [main-thread]
+   bool (*load_preset_from_file)(struct clap_plugin *plugin, const char *path);
+};
+
+#ifdef __cplusplus
+}
+#endif
