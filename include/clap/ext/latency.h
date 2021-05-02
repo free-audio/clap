0c7c587466c7f9d8cbbbc01a5dac78ff328ac859
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-05-02 20:04:29 +0200

    oups

--- include/clap/ext/latency.h
+++ include/clap/ext/latency.h
@@ -14,14 +14,14 @@ typedef struct clap_plugin_latency {
    // Returns the plugin latency.
    // [main-thread]
    uint32_t (*get_latency)(clap_plugin *plugin);
-} clap_plugin_audio_ports;
+} clap_plugin_latency;
 
-typedef struct clap_host_audio_ports {
+typedef struct clap_host_latency {
    // Tell the host that the latency changed.
    // The new latency will be effective after deactivation of the plugin.
    // [main-thread]
    void (*changed)(clap_host *host, clap_plugin *plugin, uint32_t new_latency);
-} clap_host_audio_ports;
+} clap_host_latency;
 
 #ifdef __cplusplus
 }
