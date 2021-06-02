f4d51fe22183963a4bf26b7ebc9b71c26d8e38d0
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-06-02 22:15:50 +0200

    Add check_for_update extension

---
+++ include/clap/ext/draft/check-for-update.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../clap.h"
+
+#define CLAP_EXT_CHECK_FOR_UPDATE "clap/draft/check_for_update"
+
+typedef struct clap_check_for_update_info {
+   const char *version;
+   const char *release_date; // YYYY-MM-DD
+   const char *url;
+
+   bool is_stable;
+} clap_check_for_update_info;
+
+typedef struct clap_plugin_check_for_update {
+   // [main-thread]
+   void (*check)(const clap_host *host, bool include_beta);
+} clap_plugin_check_for_update;
+
+typedef struct clap_host_check_for_update {
+   // [main-thread]
+   void (*on_new_version)(const clap_host *host, const clap_check_for_update_info *update_info);
+} clap_host_check_for_update;
+
+#ifdef __cplusplus
+}
+#endif
