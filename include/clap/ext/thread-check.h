2d66a944f42e5cf8c3260b4c961fea9558667610
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-06-02 22:26:08 +0200

    Add comment

--- include/clap/ext/thread-check.h
+++ include/clap/ext/thread-check.h
@@ -10,6 +10,7 @@ extern "C" {
 
 // This interface is useful to do runtime checks and make
 // sure that the functions are called on the correct threads.
+// It is highly recommended to implement this extension
 typedef struct clap_host_thread_check {
    // Returns true if the "this" thread is the main thread.
    // [thread-safe]
