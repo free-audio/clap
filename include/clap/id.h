663dd39135195629e2bc99087591828a0ec09729
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-09-20 21:21:18 +0200

    Make it constexpr

--- include/clap/id.h
+++ include/clap/id.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include "macros.h"
+
 typedef uint32_t clap_id;
 
-static const clap_id CLAP_INVALID_ID = UINT32_MAX;
+static const CLAP_CONSTEXPR clap_id CLAP_INVALID_ID = UINT32_MAX;
