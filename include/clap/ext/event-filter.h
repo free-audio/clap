dcbb317103ca9b5cb77238123523f920d40d8559
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-11-29 13:42:39 +0100

    Update event-filter documentation and extension name

--- include/clap/ext/event-filter.h
+++ include/clap/ext/event-filter.h
@@ -2,7 +2,7 @@
 
 #include "../clap.h"
 
-static CLAP_CONSTEXPR const char CLAP_EXT_EVENT_FILTER[] = "clap.event_filter";
+static CLAP_CONSTEXPR const char CLAP_EXT_EVENT_FILTER[] = "clap.event-filter";
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +10,8 @@ extern "C" {
 
 // This extension lets the host know which event types the plugin is interested
 // in.
+// The host will cache the set of accepted events before activating the plugin.
+// The set of accepted events can't change while the plugin is active.
 typedef struct clap_plugin_event_filter {
    // Returns true if the plugin is interested in the given event type.
    // [main-thread]
@@ -18,6 +20,8 @@ typedef struct clap_plugin_event_filter {
 
 
 typedef struct clap_host_event_filter {
+   // Informs the host that the set of accepted event type changed.
+   // This requires the plugin to be deactivated.
    // [main-thread]
    void (*changed)(const clap_host *host);
 } clap_host_event_filter;
