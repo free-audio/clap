55224a66e6c674d3820c31f0ccd3d75a6cefad20
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-05 14:55:56 +0100

    rework testing

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,6 @@
 
 // The purpose of this file is to check that all headers compile
 
-clap_version m = CLAP_VERSION;
+static const constexpr clap_version m = CLAP_VERSION;
 
 int main(int, char **) { return !clap_version_is_compatible(m); }
