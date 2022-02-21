5e7060612cababda7b944c6349ac1b0c0ed753a5
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-21 13:02:53 +0100

    Allow the host to suggest a window title

--- include/clap/ext/gui-free-standing.h
+++ include/clap/ext/gui-free-standing.h
@@ -14,7 +14,11 @@ typedef struct clap_plugin_gui_free_standing {
    // Opens the plugin window as a free standing window, which means it is not
    // embedded in the host and managed by the plugin.
    // [main-thread]
-   bool (*open)(const clap_plugin_t * plugin);
+   bool (*open)(const clap_plugin_t *plugin);
+
+   // Suggests a window title
+   // [main-thread]
+   void (*suggest_window_title)(const clap_plugin_t *plugin);
 } clap_plugin_gui_free_standing_t;
 
 #pragma pack(pop)
