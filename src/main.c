aa5e9cb8b63e39fcbf28eba95c5b722702a2a3da
Alexandre Bique <bique.alexandre@gmail.com>
2022-03-03 09:24:20 +0100

    Fix a bunch of compilation warning regarding end of file

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 // The purpose of this file is to check that all headers compile
 int main(int argc, char **argv) {
+   (void)argc;
+   (void)argv;
    clap_version_t m = CLAP_VERSION;
    return !clap_version_is_compatible(m);
 }
