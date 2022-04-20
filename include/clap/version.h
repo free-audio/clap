c6ab2cd65793c2f47bf86befd3e74aa84073863a
Alexandre Bique <bique.alexandre@gmail.com>
2022-04-20 14:37:24 +0200

    Version 0.24.1

--- include/clap/version.h
+++ include/clap/version.h
@@ -22,7 +22,7 @@ typedef struct clap_version {
 
 static CLAP_CONSTEXPR const uint32_t CLAP_VERSION_MAJOR = 0;
 static CLAP_CONSTEXPR const uint32_t CLAP_VERSION_MINOR = 24;
-static CLAP_CONSTEXPR const uint32_t CLAP_VERSION_REVISION = 0;
+static CLAP_CONSTEXPR const uint32_t CLAP_VERSION_REVISION = 1;
 
 static CLAP_CONSTEXPR const clap_version_t CLAP_VERSION = {
    CLAP_VERSION_MAJOR, CLAP_VERSION_MINOR, CLAP_VERSION_REVISION};
