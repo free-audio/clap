769d1dd3815a607841ffafaddda854aa98043dd8
Jean Pierre Cimalando <jp-dev@inbox.ru>
2022-04-25 18:37:27 +0200

    Allow CLAP_EXPORT to have a custom definition

--- include/clap/private/macros.h
+++ include/clap/private/macros.h
@@ -1,17 +1,19 @@
 #pragma once
 
 // Define CLAP_EXPORT
-#if defined _WIN32 || defined __CYGWIN__
-#   ifdef __GNUC__
-#      define CLAP_EXPORT __attribute__((dllexport))
+#if !defined(CLAP_EXPORT)
+#   if defined _WIN32 || defined __CYGWIN__
+#      ifdef __GNUC__
+#         define CLAP_EXPORT __attribute__((dllexport))
+#      else
+#         define CLAP_EXPORT __declspec(dllexport)
+#      endif
 #   else
-#      define CLAP_EXPORT __declspec(dllexport)
-#   endif
-#else
-#   if __GNUC__ >= 4 || defined(__clang__)
-#      define CLAP_EXPORT __attribute__((visibility("default")))
-#   else
-#      define CLAP_EXPORT
+#      if __GNUC__ >= 4 || defined(__clang__)
+#         define CLAP_EXPORT __attribute__((visibility("default")))
+#      else
+#         define CLAP_EXPORT
+#      endif
 #   endif
 #endif
 
