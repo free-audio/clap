1d04029b79745a26965200c0c426c28ad45b611a
Alexandre Bique <bique.alexandre@gmail.com>
2022-05-18 13:51:37 +0200

    Fixes #80

--- include/clap/ext/draft/cv.h
+++ include/clap/ext/draft/cv.h
@@ -2,7 +2,7 @@
 
 #include "../../plugin.h"
 
-// This extension can be used to specify the channel mapping used by the plugin.
+// This extension can be used to specify the cv channel type used by the plugin.
 // Work in progress, suggestions are welcome
 
 static CLAP_CONSTEXPR const char CLAP_EXT_CV[] = "clap.cv.draft/0";
@@ -19,14 +19,17 @@ enum {
    CLAP_CV_PITCH = 2,
 };
 
+// TODO: maybe we want a channel_info instead, where we could have more details about the supported
+// ranges?
+
 typedef struct clap_plugin_cv {
-   // Stores into the channel_map array, the surround identifer of each channels.
-   // Returns the number of elements stored in channel_map
+   // Returns true on success.
    // [main-thread]
-   uint32_t (*get_channel_type)(const clap_plugin_t *plugin,
-                                bool                 is_input,
-                                uint32_t             port_index,
-                                uint32_t             channel_index);
+   bool (*get_channel_type)(const clap_plugin_t *plugin,
+                            bool                 is_input,
+                            uint32_t             port_index,
+                            uint32_t             channel_index,
+                            uint32_t            *channel_type);
 } clap_plugin_cv_t;
 
 typedef struct clap_host_cv {
