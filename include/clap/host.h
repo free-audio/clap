1d9bbb895557eaf8759b54bcd87745f480a909ea
Alexandre Bique <bique.alexandre@gmail.com>
2022-05-22 09:53:50 +0200

    Documentation

--- include/clap/host.h
+++ include/clap/host.h
@@ -15,7 +15,7 @@ typedef struct clap_host {
    const char *name;    // eg: "Bitwig Studio"
    const char *vendor;  // eg: "Bitwig GmbH"
    const char *url;     // eg: "https://bitwig.com"
-   const char *version; // eg: "3.3.8"
+   const char *version; // eg: "4.3"
 
    // Query an extension.
    // [thread-safe]
--- include/clap/plugin-factory.h
+++ include/clap/plugin-factory.h
@@ -13,22 +13,22 @@ extern "C" {
 //
 // If the content of the factory may change due to external events, like the user installed
 typedef struct clap_plugin_factory {
-   /* Get the number of plugins available.
-    * [thread-safe] */
+   // Get the number of plugins available.
+   // [thread-safe]
    uint32_t (*get_plugin_count)(const struct clap_plugin_factory *factory);
 
-   /* Retrieves a plugin descriptor by its index.
-    * Returns null in case of error.
-    * The descriptor must not be freed.
-    * [thread-safe] */
+   // Retrieves a plugin descriptor by its index.
+   // Returns null in case of error.
+   // The descriptor must not be freed.
+   // [thread-safe]
    const clap_plugin_descriptor_t *(*get_plugin_descriptor)(
       const struct clap_plugin_factory *factory, uint32_t index);
 
-   /* Create a clap_plugin by its plugin_id.
-    * The returned pointer must be freed by calling plugin->destroy(plugin);
-    * The plugin is not allowed to use the host callbacks in the create method.
-    * Returns null in case of error.
-    * [thread-safe] */
+   // Create a clap_plugin by its plugin_id.
+   // The returned pointer must be freed by calling plugin->destroy(plugin);
+   // The plugin is not allowed to use the host callbacks in the create method.
+   // Returns null in case of error.
+   // [thread-safe]
    const clap_plugin_t *(*create_plugin)(const struct clap_plugin_factory *factory,
                                          const clap_host_t                *host,
                                          const char                       *plugin_id);
