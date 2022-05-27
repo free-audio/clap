32fcc834d5dae7b24eb6a71fd5fc5e1194b33c2d
Alexandre Bique <bique.alexandre@gmail.com>
2022-05-27 11:11:26 +0200

    doc

--- include/clap/stream.h
+++ include/clap/stream.h
@@ -10,17 +10,14 @@ extern "C" {
 typedef struct clap_istream {
    void *ctx; // reserved pointer for the stream
 
-   /* returns the number of bytes read.
-    * 0 for end of file.
-    * -1 on error. */
+   // returns the number of bytes read; 0 indicates end of file and -1 a read error
    int64_t (*read)(const struct clap_istream *stream, void *buffer, uint64_t size);
 } clap_istream_t;
 
 typedef struct clap_ostream {
    void *ctx; // reserved pointer for the stream
 
-   /* returns the number of bytes written.
-    * -1 on error. */
+   // returns the number of bytes written; -1 on write error
    int64_t (*write)(const struct clap_ostream *stream, const void *buffer, uint64_t size);
 } clap_ostream_t;
 
