84ad6fe9739d3070ed0eb05c24be303a899ab813
Alexandre Bique <bique.alexandre@gmail.com>
2022-06-03 17:48:46 +0200

    Version 1.0

--- include/clap/version.h
+++ include/clap/version.h
@@ -20,16 +20,15 @@ typedef struct clap_version {
 }
 #endif
 
-#define CLAP_VERSION_MAJOR ((uint32_t)0)
-#define CLAP_VERSION_MINOR ((uint32_t)26)
+#define CLAP_VERSION_MAJOR ((uint32_t)1)
+#define CLAP_VERSION_MINOR ((uint32_t)0)
 #define CLAP_VERSION_REVISION ((uint32_t)0)
 #define CLAP_VERSION_INIT {CLAP_VERSION_MAJOR, CLAP_VERSION_MINOR, CLAP_VERSION_REVISION}
 
 static const CLAP_CONSTEXPR clap_version_t CLAP_VERSION = CLAP_VERSION_INIT;
 
-// For version 0, we require the same minor version because
-// we may still break the ABI at this point
 CLAP_NODISCARD static inline CLAP_CONSTEXPR bool
 clap_version_is_compatible(const clap_version_t v) {
-   return v.major == CLAP_VERSION_MAJOR && v.minor == CLAP_VERSION_MINOR;
+   // versions 0.x.y were used during development stage and aren't compatible
+   return v.major >= 1;
 }
