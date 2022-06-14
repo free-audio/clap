871163b6e8f5586e9f83b3f575e986d5b689e917
Alexandre Bique <bique.alexandre@gmail.com>
2022-06-14 09:29:06 +0200

    Use param_info.module in compile test

--- src/main.cc
+++ src/main.cc
@@ -4,4 +4,9 @@
 
 static const constexpr clap_version m = CLAP_VERSION;
 
-int main(int, char **) { return !clap_version_is_compatible(m); }
+int main(int, char **) {
+   clap_param_info param_info;
+   param_info.module[0] = 'm';
+
+   return !clap_version_is_compatible(m);
+}
