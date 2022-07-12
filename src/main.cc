7abef7109cbfe84426dcfbe00a0af31940684273
Alexandre Bique <bique.alexandre@gmail.com>
2022-07-12 11:27:34 +0200

    Fix warning, this chunk of code wasn't very useful.

--- src/main.cc
+++ src/main.cc
@@ -5,8 +5,5 @@
 static const constexpr clap_version m = CLAP_VERSION;
 
 int main(int, char **) {
-   clap_param_info param_info;
-   param_info.module[0] = 'm';
-
    return !clap_version_is_compatible(m);
 }
