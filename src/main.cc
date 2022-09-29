257f1b2c29277acdf8c5fc05551f586141d981bb
Alexandre Bique <bique.alexandre@gmail.com>
2022-09-29 19:07:22 +0200

    Fixes #181

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 
 // The purpose of this file is to check that all headers compile
 
-static const constexpr clap_version m = CLAP_VERSION;
+static const CLAP_CONSTEXPR clap_version m = CLAP_VERSION;
 
 int main(int, char **) {
    return !clap_version_is_compatible(m);
