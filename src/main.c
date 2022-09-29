467643440d2a7fe21ebe2ecfd2e429dd390c51bd
Alexandre Bique <bique.alexandre@gmail.com>
2022-09-29 19:07:56 +0200

    make it const

--- src/main.c
+++ src/main.c
@@ -4,6 +4,6 @@
 int main(int argc, char **argv) {
    (void)argc;
    (void)argv;
-   clap_version_t m = CLAP_VERSION;
+   const clap_version_t m = CLAP_VERSION;
    return !clap_version_is_compatible(m);
 }
