26054d0ee33737a81cea5c27eeb6c385304d3000
Alexandre Bique <bique.alexandre@gmail.com>
2022-11-28 13:53:12 +0100

    Fix symbols

--- include/clap/ext/draft/remote-controls.h
+++ include/clap/ext/draft/remote-controls.h
@@ -55,10 +55,10 @@ typedef struct clap_plugin_remote_controls {
    bool(CLAP_ABI *get)(const clap_plugin_t        *plugin,
                        uint32_t                    page_index,
                        clap_remote_controls_page_t *page);
-} clap_plugin_quick_controls_t;
+} clap_plugin_remote_controls_t;
 
-typedef struct clap_host_quick_controls {
-   // Informs the host that the quick controls have changed.
+typedef struct clap_host_remotecontrols {
+   // Informs the host that the remote controls have changed.
    // [main-thread]
    void(CLAP_ABI *changed)(const clap_host_t *host);
 
@@ -66,7 +66,7 @@ typedef struct clap_host_quick_controls {
    // plugin's GUI.
    // [main-thread]
    void(CLAP_ABI *suggest_page)(const clap_host_t *host, clap_id page_id);
-} clap_host_quick_controls_t;
+} clap_host_remote_controls_t;
 
 #ifdef __cplusplus
 }
