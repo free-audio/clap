3c75ee5bf7e2eb62e275d69795c7bcbce8ce2296
Alexandre Bique <bique.alexandre@gmail.com>
2022-11-28 20:55:38 +0100

    revert back to track info

--- include/clap/ext/draft/channel-info.h
+++ include/clap/ext/draft/channel-info.h
@@ -4,26 +4,26 @@
 #include "../../color.h"
 #include "../../string-sizes.h"
 
-static CLAP_CONSTEXPR const char CLAP_EXT_CHANNEL_INFO[] = "clap.channel-info.draft/0";
+static CLAP_CONSTEXPR const char CLAP_EXT_TRACK_INFO[] = "clap.track-info.draft/0";
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 enum {
-   CLAP_CHANNEL_INFO_HAS_TRACK_ID = (1 << 0),
-   CLAP_CHANNEL_INFO_HAS_TRACK_NAME = (1 << 1),
-   CLAP_CHANNEL_INFO_HAS_TRACK_PATH = (1 << 2),
-   CLAP_CHANNEL_INFO_HAS_TRACK_COLOR = (1 << 3),
-   CLAP_CHANNEL_INFO_HAS_DEVICE_CHAIN_ID = (1 << 4),
-   CLAP_CHANNEL_INFO_HAS_DEVICE_CHAIN_NAME = (1 << 5),
-   CLAP_CHANNEL_INFO_HAS_DEVICE_CHAIN_PATH = (1 << 6),
-   CLAP_CHANNEL_INFO_HAS_DEVICE_CHAIN_COLOR = (1 << 7),
-   CLAP_CHANNEL_INFO_HAS_AUDIO_CHANNEL_COUNT = (1 << 8),
-   CLAP_CHANNEL_INFO_HAS_AUDIO_PORT_TYPE = (1 << 9),
+   CLAP_TRACK_INFO_HAS_TRACK_ID = (1 << 0),
+   CLAP_TRACK_INFO_HAS_TRACK_NAME = (1 << 1),
+   CLAP_TRACK_INFO_HAS_TRACK_PATH = (1 << 2),
+   CLAP_TRACK_INFO_HAS_TRACK_COLOR = (1 << 3),
+   CLAP_TRACK_INFO_HAS_DEVICE_CHAIN_ID = (1 << 4),
+   CLAP_TRACK_INFO_HAS_DEVICE_CHAIN_NAME = (1 << 5),
+   CLAP_TRACK_INFO_HAS_DEVICE_CHAIN_PATH = (1 << 6),
+   CLAP_TRACK_INFO_HAS_DEVICE_CHAIN_COLOR = (1 << 7),
+   CLAP_TRACK_INFO_HAS_AUDIO_CHANNEL_COUNT = (1 << 8),
+   CLAP_TRACK_INFO_HAS_AUDIO_PORT_TYPE = (1 << 9),
 };
 
-typedef struct clap_channel_info {
+typedef struct clap_track_info {
    uint32_t flags; // see the flags above
 
    // Info relatives to the track to which the current channel belong
@@ -63,19 +63,19 @@ typedef struct clap_channel_info {
    // see audio-ports.h, struct clap_audio_port_info to learn how to use channel count and port type
    int32_t     audio_channel_count;
    const char *audio_port_type;
-} clap_channel_info_t;
+} clap_track_info_t;
 
-typedef struct clap_plugin_channel_info {
-   // Called when the channel info changes.
+typedef struct clap_plugin_track_info {
+   // Called when the info changes.
    // [main-thread]
    void(CLAP_ABI *changed)(const clap_plugin_t *plugin);
-} clap_plugin_channel_info_t;
+} clap_plugin_track_info_t;
 
-typedef struct clap_host_channel_info {
+typedef struct clap_host_track_info {
    // Get info about the track the plugin belongs to.
    // [main-thread]
-   bool(CLAP_ABI *get)(const clap_host_t *host, clap_channel_info_t *info);
-} clap_host_channel_info_t;
+   bool(CLAP_ABI *get)(const clap_host_t *host, clap_track_info_t *info);
+} clap_host_track_info_t;
 
 #ifdef __cplusplus
 }
