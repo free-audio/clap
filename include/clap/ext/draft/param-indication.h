02f616acfeed1547a0f4b52a46db8d24d7345f66
Alexandre Bique <bique.alexandre@gmail.com>
2022-12-12 15:11:29 +0100

    param-indication: simplify the doc and add clear() function

--- include/clap/ext/draft/param-indication.h
+++ include/clap/ext/draft/param-indication.h
@@ -6,9 +6,8 @@
 // This extension lets the host tell the plugin that a parameter is currently
 // mapped to a physical controller.
 //
-// It is common that those controllers works using a bank of 8 knobs, and to
-// improve readability the host may assign a different color to each physical
-// knobs and indicate them on the GUI.
+// It is common to work using a bank of 8 knobs, and to improve readability the host may assign a
+// different color to each physical knobs and indicate them on the GUI.
 
 static CLAP_CONSTEXPR const char CLAP_EXT_PARAM_INDICATION[] = "clap.param-indication.draft/0";
 
@@ -20,11 +19,16 @@ typedef struct clap_plugin_param_indication {
    // Sets or clears a mapping indication.
    // If the host doesn't use mapping color, then mapping_color should be null.
    // If is_mapped is false, then the mapping color is ignored.
+   //
+   // Parameter mapping indications should not be saved in the plugin context, and are off by default.
    // [main-thread]
-   void(CLAP_ABI *set_indication)(const clap_plugin_t *plugin,
-                                  clap_id              param_id,
-                                  bool                 is_mapped,
-                                  const clap_color_t  *mapping_color);
+   void(CLAP_ABI *set)(const clap_plugin_t *plugin,
+                       clap_id              param_id,
+                       bool                 is_mapped,
+                       const clap_color_t  *mapping_color);
+
+   // Clears all parameters mapping indications
+   void(CLAP_ABI *clear)(const clap_plugin_t *plugin);
 } clap_plugin_param_indication_t;
 
 #ifdef __cplusplus
