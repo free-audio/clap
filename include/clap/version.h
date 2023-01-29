f63add06bb1137e3211b7ca06dffd1e27051f798
Alexandre Bique <bique.alexandre@gmail.com>
2023-01-29 11:41:51 +0100

    begin 1.1.8

--- include/clap/version.h
+++ include/clap/version.h
@@ -22,7 +22,7 @@ typedef struct clap_version {
 
 #define CLAP_VERSION_MAJOR 1
 #define CLAP_VERSION_MINOR 1
-#define CLAP_VERSION_REVISION 7
+#define CLAP_VERSION_REVISION 8
 
 #define CLAP_VERSION_INIT                                                                          \
    { (uint32_t)CLAP_VERSION_MAJOR, (uint32_t)CLAP_VERSION_MINOR, (uint32_t)CLAP_VERSION_REVISION }
