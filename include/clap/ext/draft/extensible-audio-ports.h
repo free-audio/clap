e6a5acbac7878865a39457c39abfac76410c6f68
Alexandre Bique <bique.alexandre@gmail.com>
2023-03-06 16:45:01 +0100

    Doc and swap arguments.

--- include/clap/ext/draft/extensible-audio-ports.h
+++ include/clap/ext/draft/extensible-audio-ports.h
@@ -13,12 +13,13 @@ static CLAP_CONSTEXPR const char CLAP_EXT_EXTENSIBLE_AUDIO_PORTS[] =
 typedef struct clap_plugin_extensible_audio_ports {
    // Asks the plugin to add a new port (at the end of the list), with the following settings.
    // Returns true on success.
+   // port_type and port_details are analogous to what you'll find in audio-ports.h
    // [main-thread && !is_active]
    bool(CLAP_ABI *add_port)(const clap_plugin_t *plugin,
                             bool                 is_input,
+                            uint32_t             channel_count,
                             const char          *port_type,
-                            const void          *port_details,
-                            uint32_t             channel_count);
+                            const void          *port_details);
 
    // Asks the plugin to remove a port.
    // Returns true on success.
