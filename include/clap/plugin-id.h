955046cd0f36da65732f9a299db8edbf17ebce6d
Alexandre Bique <bique.alexandre@gmail.com>
2024-01-16 10:09:31 +0100

    Add clarifications regarding foreign plugin id

--- include/clap/plugin-id.h
+++ include/clap/plugin-id.h
@@ -3,11 +3,15 @@
 // Pair of plugin ABI and plugin identifier.
 typedef struct clap_plugin_id {
    // The plugin ABI name, in lowercase.
-   // eg: "clap"
+   // eg: "clap", "vst3", "vst2", "au", "lv2", ...
    const char *abi;
 
    // The plugin ID, for example "com.u-he.Diva".
    // If the ABI rely upon binary plugin ids, then they shall be hex encoded (lower case).
+   // eg:
+   //     T binary_id;
+   //     const uint8_t * data = (const uint8_t *)&binary_id;
+   //     char *buffer = malloc(2 * sizeof(binary_id) + 1);
+   //     hex_encode(data, sizeof (binary_id), buffer);
    const char *id;
 } clap_plugin_id_t;
-
