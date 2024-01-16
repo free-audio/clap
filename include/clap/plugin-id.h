583177dff0aa25d6ecbfac23783534e89aac4d3f
Alexandre Bique <bique.alexandre@gmail.com>
2024-01-16 10:22:02 +0100

    Leave a note regarding endianness

--- include/clap/plugin-id.h
+++ include/clap/plugin-id.h
@@ -13,5 +13,8 @@ typedef struct clap_plugin_id {
    //     const uint8_t * data = (const uint8_t *)&binary_id;
    //     char *buffer = malloc(2 * sizeof(binary_id) + 1);
    //     hex_encode(data, sizeof (binary_id), buffer);
+   //
+   // Note: if the binary id is sensible to the CPU endianness, the encoded id will be too.
+   // As a result, the id should be decoded immediately by the consumer.
    const char *id;
 } clap_plugin_id_t;
