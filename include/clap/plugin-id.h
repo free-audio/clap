c53c0a7f65e322f120788ec8b573a2f1b05cb3f4
Alexandre Bique <bique.alexandre@gmail.com>
2024-01-17 12:20:02 +0100

    Specify how to format foreign plugin id

--- include/clap/plugin-id.h
+++ include/clap/plugin-id.h
@@ -6,17 +6,18 @@ typedef struct clap_plugin_id {
    // eg: "clap", "vst3", "vst2", "au", "lv2", ...
    const char *abi;
 
-   // The plugin ID, for example "com.u-he.Diva".
-   // If the ABI rely upon binary plugin ids, then they shall be hex encoded (lower case).
-   // eg:
-   //     T binary_id;
-   //     const uint8_t * data = (const uint8_t *)&binary_id;
-   //     const size_t buffer_size = 2 * sizeof(binary_id) + 1;
-   //     char *buffer = malloc(buffer_size);
-   //     hex_encode(data, sizeof (binary_id), buffer);
-   //     buffer[buffer_size - 1] = '\0';
+   // The plugin ID, formatted as follow:
    //
-   // Note: if the binary id is sensible to the CPU endianness, the encoded id will be too.
-   // As a result, the id should be decoded immediately by the consumer.
+   // CLAP: use the plugin id
+   //   eg: "com.u-he.diva"
+   //
+   // AU: format the string like "manu:type:subt"
+   //   eg: "aumu:SgXT:VmbA"
+   //
+   // VST2: print the id as a signed 32-bits integer
+   //   eg: "-4382976"
+   //
+   // VST3: print the id as a standard UUID
+   //   eg: "123e4567-e89b-12d3-a456-426614174000"
    const char *id;
 } clap_plugin_id_t;
