8323ec1298329e4f5ead35f5669fef365492b69c
trinitou <debuggleburger@gmail.com>
2024-02-07 23:31:09 +0100

    Adjust latency extension requirements
    - allow plugin_latency->get to be called during plugin->activate
    - require host_latency->changed to be called during plugin->activate

--- include/clap/ext/latency.h
+++ include/clap/ext/latency.h
@@ -10,15 +10,15 @@ extern "C" {
 
 typedef struct clap_plugin_latency {
    // Returns the plugin latency in samples.
-   // [main-thread & active]
+   // [main-thread & (being-activated | active)]
    uint32_t(CLAP_ABI *get)(const clap_plugin_t *plugin);
 } clap_plugin_latency_t;
 
 typedef struct clap_host_latency {
    // Tell the host that the latency changed.
-   // The latency is only allowed to change if the plugin is deactivated.
+   // The latency is only allowed to change during plugin->activate.
    // If the plugin is activated, call host->request_restart()
-   // [main-thread]
+   // [main-thread & being-activated]
    void(CLAP_ABI *changed)(const clap_host_t *host);
 } clap_host_latency_t;
 
