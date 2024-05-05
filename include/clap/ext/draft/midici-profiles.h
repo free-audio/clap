1fa02a323bc856d6446b446da85eb401dc1669a5
Bremmers <bremmers@xs4all.nl>
2024-05-05 12:57:26 +0200

    C fixes

--- include/clap/ext/draft/midici-profiles.h
+++ include/clap/ext/draft/midici-profiles.h
@@ -57,18 +57,18 @@ enum {
 };
 
 typedef struct profile_id_t {
-   byte_t id;
-   byte_t bank;
-   byte_t number;
-   byte_t version;
-   byte_t level;
+   uint8_t id;
+   uint8_t bank;
+   uint8_t number;
+   uint8_t version;
+   uint8_t level;
 };
 
 typedef struct profile_t {
    profile_id_t profile_id;
-   byte_t       channel;        // see "destination addressing" paragraph at top of file.
+   uint8_t      channel;        // see "destination addressing" paragraph at top of file.
    uint16_t     num_channels;   // see "destination addressing" paragraph at top of file.
-   byte_t       enabled;        // CLAP_MIDICI_PROFILE_*
+   uint8_t      enabled;        // CLAP_MIDICI_PROFILE_*
 };
 
 typedef struct clap_plugin_midici_profiles {
@@ -95,13 +95,13 @@ typedef struct clap_plugin_midici_profiles {
    // The profile targeted by channel/num_channels should generally be enabled before calling this function.
    // In some cases profile detals like available channels are needed before enabling a profile. Plugins should make sure this is handled correctly.
    // [main-thread]
-   bool(CLAP_ABI *get_details)(const clap_plugin_t    *plugin,
-                               uint16_t                port_index,
-                               const profile_id_t      profile,
-                               byte_t                  channel,
-                               uint16_t                num_channels,
-                               uint8_t                 inquiry_target,
-                               const clap_ostream_t   *stream);
+   bool(CLAP_ABI *get_details)(const clap_plugin_t      *plugin,
+                               uint16_t                  port_index,
+                               const struct profile_id_t profile,
+                               uint8_t                   channel,
+                               uint16_t                  num_channels,
+                               uint8_t                   inquiry_target,
+                               const clap_ostream_t     *stream);
 
    // Send Profile Specific Data to profile at channel/num_channels.
    // Returns true if data was accepted
@@ -109,13 +109,13 @@ typedef struct clap_plugin_midici_profiles {
    // Example: MPE Channel Response Type Notification would be F0 01 F7
    // A profile may specify multiple data messages. It's recommended to send the complete 'state' in a single set_data() call.
    // [main-thread]
-   bool(CLAP_ABI *set_data)(const clap_plugin_t   *plugin,
-                            uint16_t               port_index,
-                            const profile_id_t     profile,
-                            byte_t                 channel,
-                            uint16_t               num_channels,
-                            const uint8_t         *buffer,
-                            uint32_t               buffer_size);
+   bool(CLAP_ABI *set_data)(const clap_plugin_t      *plugin,
+                            uint16_t                  port_index,
+                            const struct profile_id_t profile,
+                            uint8_t                   channel,
+                            uint16_t                  num_channels,
+                            const uint8_t            *buffer,
+                            uint32_t                  buffer_size);
 
    // Enables a profile at channel/num_channels.
    // Returns true if the profile is enabled at channel/num_channels when the function returns.
@@ -124,22 +124,22 @@ typedef struct clap_plugin_midici_profiles {
    // Note for hosts: after calling this function count()/get() may have changed !!
    // Note for plugins: do not call clap_host_midici_profiles.changed.
    // [main-thread]
-   bool(CLAP_ABI *enable)(const clap_plugin_t   *plugin,
-                          uint16_t               port_index,
-                          const profile_id_t     profile,
-                          byte_t                 channel,
-                          uint16_t               num_channels);
+   bool(CLAP_ABI *enable)(const clap_plugin_t      *plugin,
+                          uint16_t                  port_index,
+                          const struct profile_id_t profile,
+                          uint8_t                   channel,
+                          uint16_t                  num_channels);
 
    // Disables a profile at channel/num_channels.
    // Returns true if the profile isn't enabled at channel/num_channels when the function returns.
    // Note for hosts: after calling this function count()/get() may have changed !!
    // Note for plugins: do not call clap_host_midici_profiles.changed.
    // [main-thread]
-   bool(CLAP_ABI *disable)(const clap_plugin_t   *plugin,
-                           uint16_t               port_index,
-                           const profile_id_t     profile,
-                           byte_t                 channel,
-                           uint16_t               num_channels);
+   bool(CLAP_ABI *disable)(const clap_plugin_t      *plugin,
+                           uint16_t                  port_index,
+                           const struct profile_id_t profile,
+                           uint8_t                   channel,
+                           uint16_t                  num_channels);
 } clap_plugin_midici_profiles_t;
 
 typedef struct clap_host_midici_profiles {
@@ -150,11 +150,11 @@ typedef struct clap_host_midici_profiles {
    // Plugins calls this if host needs to read Profile Specific Data Messages again.
    // Host calls get_details(.., port_index, profile, channel, num_channels, CLAP_MIDICI_PROFILE_INQUIRY_TARGET_DATA, ..).
    // [main-thread]
-   void(CLAP_ABI *datachanged)(const clap_host_t   *host,
-                               uint16_t             port_index,
-                               const profile_id_t   profile,
-                               byte_t               channel,
-                               uint16_t             num_channels);
+   void(CLAP_ABI *datachanged)(const clap_host_t       *host,
+                               uint16_t                 port_index,
+                               const structprofile_id_t profile,
+                               uint8_t                  channel,
+                               uint16_t                 num_channels);
 } clap_host_midici_profiles_t;
 
 #ifdef __cplusplus
