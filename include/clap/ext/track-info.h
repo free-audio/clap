1bd5636ea1e6ceba2858c63d86f3976d588e0410
Roelof van Krimpen <roelof@fabfilter.com>
2024-05-06 10:47:20 +0200

    Revert previous change to track-info extension.

--- include/clap/ext/track-info.h
+++ include/clap/ext/track-info.h
@@ -31,9 +31,6 @@ enum {
 
    // This plugin is on the master, initialize with appropriate settings for channel processing
    CLAP_TRACK_INFO_IS_FOR_MASTER = (1 << 5),
-
-   // The clap_track_info_t struct contains a track_index. 
-   CLAP_TRACK_INFO_HAS_TRACK_INDEX = (1 << 6),
 };
 
 typedef struct clap_track_info {
@@ -50,8 +47,6 @@ typedef struct clap_track_info {
    int32_t     audio_channel_count;
    const char *audio_port_type;
 
-   // Track index within the host, available if flags contain CLAP_TRACK_INFO_HAS_TRACK_INDEX.
-   int32_t track_index;
 } clap_track_info_t;
 
 typedef struct clap_plugin_track_info {
