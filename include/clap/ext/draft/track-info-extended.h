a3c28cb87ee6e2d3d852c733e1fada87903a8172
Roelof van Krimpen <roelof@fabfilter.com>
2024-05-06 14:48:22 +0200

    Clean up get-/set_string interface.

--- include/clap/ext/draft/track-info-extended.h
+++ include/clap/ext/draft/track-info-extended.h
@@ -26,16 +26,26 @@ typedef struct clap_host_track_info_extended {
    // Gets a value with a given key from the host, fills 'value' with the 
    // corresponding value and returns true if a value was filled succesfully. 
    // If it returns false, the value of 'value' isn't touched.
+   // 
+   // For the string value getter specifically, the get function should return the 
+   // size of the value it's written (or would've written if it had fit in the 
+   // buffer), excluding the null terminator. 
+   // If there's no value matching the key, a value of 0 is returned. If no value 
+   // pointer is provided and/or value_capacity is 0, the value shouldn't be touched, 
+   // but a correct required length should be returned.
+   // .
    // [main-thread]
    bool(CLAP_ABI *get_int)(const clap_host_t *host, const char *key, int32_t *value);
-   bool(CLAP_ABI *get_string)(const clap_host_t *host, const char *key, char (*value)[CLAP_NAME_SIZE]);
    bool(CLAP_ABI *get_double)(const clap_host_t *host, const char *key, double *value);
+   int32_t(CLAP_ABI *get_string)(const clap_host_t *host, const char *key, char *value, int32_t value_capacity);
 
    // Sets a value with a given key. Returns true if the value was set succesfully.
+   // Note that string values provided are null-terminated and shouldn't be 
+   // expected to have a lifetime past the set_string call.
    // [main-thread]
    bool(CLAP_ABI *set_int)(const clap_host_t *host, const char *key, int32_t value);
-   bool(CLAP_ABI *set_string)(const clap_host_t *host, const char *key, char (value)[CLAP_NAME_SIZE]);
    bool(CLAP_ABI *set_double)(const clap_host_t *host, const char *key, double value);
+   bool(CLAP_ABI *set_string)(const clap_host_t *host, const char *key, const char *value);
 
 } clap_host_track_info_extended_t;
 
