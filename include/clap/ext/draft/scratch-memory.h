a5790e1b3538331b777a605fcb12be1071d9647b
jatin <jatinchowdhury18@gmail.com>
2024-10-08 11:52:53 -0700

    Very rough draft scratch-memory extension

---
+++ include/clap/ext/draft/scratch-memory.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../../plugin.h"
+
+// This extension lets the plugin request "scratch" memory
+// from the host. Scratch memory can be accessed during the
+// `process()` callback, but is not persistent between callbacks.
+
+static CLAP_CONSTEXPR const char CLAP_EXT_SCRATCH_MEMORY[] = "clap.scratch-memory/1";
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct clap_host_scratch_memory {
+   // Asks the host for certain amount of scratch memory.
+   // If the host is unable to provide the memory, it should
+   // return "false".
+   // [main-thread & being-activated]
+   bool(CLAP_ABI *request_scratch_size)(const clap_host_t *host, size_t scratch_size_bytes);
+
+   // Asks the host for the previously requested scratch memory.
+   // If the host returned "true" when scratch memory was requested,
+   // then this method must return a pointer to a memory block at least
+   // as large as the requested size. If the host returned "false"
+   // when scratch memory was requested then this method should return
+   // null.
+   //
+   // Note that this memory may be uninitialized, so the plugin may
+   // want to "zero" the memory before using it.
+   //
+   // [audio-thread]
+   void*(CLAP_ABI *access_scratch)(const clap_host_t *host);
+};
+
+#ifdef __cplusplus
+}
+#endif
