33093295cca08dce9e56c26462017ba18edc2bc6
Geraint Luff <geraint@signalsmith-audio.co.uk>
2024-12-02 15:03:54 +0000

    Clarified wording

--- include/clap/ext/draft/webview.h
+++ include/clap/ext/draft/webview.h
@@ -21,7 +21,7 @@ extern "C" {
 
 typedef struct clap_plugin_webview {
    // Returns the URL for the webview's initial navigation, as a null-terminated UTF-8 string.
-   // This must be called at least once before any messages are sent (or accepted by the host).
+   // This must be called while the webview is not open, and must be called before it is opened.
    // If this URL is relative, it is resolved relative to the plugin (bundle) resource directory.
    // The host may assume that no resources outside of that directory are used, and may use any
    // protocol to provide this content, following HTTP-like relative URL resolution. The page must
@@ -33,7 +33,7 @@ typedef struct clap_plugin_webview {
                              char                *out_buffer,
                              uint32_t            out_buffer_capacity);
 
-   // Receives a single message from the webview, which must be open and ready to receive replies.
+   // Receives a single message from the webview, which must be open.
    // Returns true on success.
    // [main-thread]
    bool(CLAP_ABI *receive)(const clap_plugin_t *plugin, const void *buffer, uint32_t size);
@@ -41,14 +41,15 @@ typedef struct clap_plugin_webview {
 } clap_plugin_webview_t;
 
 typedef struct clap_host_webview {
-   // Checks whether the webview is open and likely to receive messages.
-   // This may be called at any time and is a hint for non-essential work, such as being able to
-   // skip metering calculations.
+   // Returns whether the webview is "open" (running and ready to receive messages).
+   // If called from the main thread, it must return the open state specified by other methods.
+   // If called from any other thread, it must only be used as a hint for non-essential work (such
+   // as to skip metering calculations) as there are no synchronisation guarantees.
    // [thread-safe]
    bool(CLAP_ABI *is_open)(const clap_host_t *host);
 
    // Sends a single message to the webview.
-   // Returns true on success. It must fail (false) if the webview is not open.
+   // Returns true on success. Should fail if the webview is not open.
    // [main-thread]
    bool(CLAP_ABI *send)(const clap_host_t *host, const void *buffer, uint32_t size);
 
