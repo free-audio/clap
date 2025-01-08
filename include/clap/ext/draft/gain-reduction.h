b7a3fba3578e6d94a361fd242c09991876b706e4
John Schwartz <schwa@cockos.com>
2025-01-08 11:14:09 -0500

    add documentation, rename accessor function

--- include/clap/ext/draft/gain-reduction.h
+++ include/clap/ext/draft/gain-reduction.h
@@ -11,9 +11,20 @@ extern "C" {
 #endif
 
 typedef struct clap_plugin_gain_reduction {
-  // reports the current gain reduction in dB. the value is intended
-  // for informational display only, for example in a host meter or tooltip.
-   double(CLAP_ABI *current_gain_reduction_db)(const clap_plugin_t *plugin);
+  // Returns the current gain reduction in dB. the value is intended
+  // for informational display, for example in a host meter or tooltip.
+  //
+  // The function should be called on the audio thread to ensure consistency
+  // with processing time, although the host will likely use the value on
+  // the main thread.
+  //
+  // The returned value is in DB. Zero means the plugin is applying no gain
+  // reduction, or is not processing. A negative value means the plugin is
+  // applying gain reduction. A positive value means the plugin is adding
+  // gain. A single value is returned for all audio channels.
+  //
+  // [audio-thread]
+  double(CLAP_ABI *get)(const clap_plugin_t *plugin);
 } clap_plugin_gain_reduction_t;
 
 #ifdef __cplusplus
