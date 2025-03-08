41e83c9182f54febb916d4de6350b785fda5b2a2
Alexandre Bique <bique.alexandre@gmail.com>
2025-03-08 16:54:59 +0100

    Merge mini cure display and the hints extension

--- include/clap/ext/draft/mini-curve-display.h
+++ include/clap/ext/draft/mini-curve-display.h
@@ -11,6 +11,50 @@ static CLAP_CONSTEXPR const char CLAP_EXT_MINI_CURVE_DISPLAY[] = "clap.mini-curv
 extern "C" {
 #endif
 
+enum clap_mini_curve_display_curve_kind {
+   // The mini curve is intended to draw the total gain response of the plugin.
+   // In this case the y values are in dB and the x values are in hz (logarithmic).
+   // This would be useful in for example an equalizer.
+   CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_GAIN_RESPONSE = 1,
+
+   // The mini curve is intended to draw the total phase response of the plugin.
+   // In this case the y values are in radians and the x values are in hz (logarithmic).
+   // This would be useful in for example an equalizer.
+   CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_PHASE_RESPONSE = 2,
+
+   // The mini curve is intended to draw the transfer curve of the plugin.
+   // In this case the both x and y values are in dB.
+   // This would be useful in for example a compressor or distortion plugin.
+   CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_TRANSFER_CURVE = 3,
+
+   // This mini curve is intended to draw gain reduction over time. In this case
+   // x refers to the window in seconds and y refers to level in dB, x_min is
+   // always 0, and x_max would be the duration of the window.
+   // This would be useful in for example a compressor or limiter.
+   CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_GAIN_REDUCTION = 4,
+
+   // This curve is intended as a generic time series plot. In this case
+   // x refers to the window in seconds. x_min is always 0, and x_max would be the duration of the
+   // window.
+   // Y is not specified and up to the plugin.
+   CLAP_MINI_CURVE_DISPLAY_CURVE_KIND_TIME_SERIES = 5,
+
+   // Note: more entries could be added here in the future
+};
+
+typedef struct clap_mini_display_curve_hints {
+
+   // Range for the x axis.
+   double x_min;
+   double x_max;
+
+   // Range for the y axis.
+   double y_min;
+   double y_max;
+
+} clap_mini_display_curve_hints_t;
+
+
 typedef struct clap_plugin_mini_curve_display {
    // Renders the curve into the data buffer.
    // The value 0 will be at the bottom of the curve and UINT16_MAX will be at the top.
@@ -49,6 +93,13 @@ enum clap_mini_curve_display_change_flags {
 };
 
 typedef struct clap_host_mini_curve_display {
+   // Fills in the given clap_mini_display_curve_hints_t structure and returns
+   // true if succesful. If not, return false.
+   // [main-thread]
+   bool(CLAP_ABI *get)(const clap_host_t               *host,
+      uint32_t                         kind,
+      clap_mini_display_curve_hints_t *hints);
+
    // Mark the curve as being static or dynamic.
    // The curve is initially considered as static, though the plugin should explicitely
    // initialize this state.
