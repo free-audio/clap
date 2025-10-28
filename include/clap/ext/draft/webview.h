ea98345ed931b4d624f62f0f47d531431108f9ae
Geraint Luff <geraint@signalsmith-audio.co.uk>
2025-10-28 14:53:22 +0000

    `clap.webview/3`: add `.get_resource()` function for serving from memory

--- include/clap/ext/draft/webview.h
+++ include/clap/ext/draft/webview.h
@@ -1,9 +1,9 @@
 #pragma once
 
-static CLAP_CONSTEXPR const char CLAP_EXT_WEBVIEW[] = "clap.webview/2";
+static CLAP_CONSTEXPR const char CLAP_EXT_WEBVIEW[] = "clap.webview/3";
 
 // clap.gui API constant. The pointer in clap_window must be NULL, but sizing methods are useful.
-// uses logical size, don't call clap_plugin_gui->set_scale()
+// This uses logical size, don't call clap_plugin_gui->set_scale()
 static const CLAP_CONSTEXPR char CLAP_WINDOW_API_WEBVIEW[] = "webview";
 
 #ifdef __cplusplus
@@ -21,18 +21,23 @@ extern "C" {
 
 typedef struct clap_plugin_webview {
    // Returns the URL for the webview's initial navigation, as a null-terminated UTF-8 string.
-   // This must be called at least once before any messages are sent (or accepted by the host).
+   // This must be called at least once before any messages are sent (or accepted) by the host.
    // Absolute URIs (including `data:`, and `file:` URIs on local systems) are always supported.
-   // If this CLAP is a bundle with resources, the URL may be relative and must be resolved
-   // relative to a `file:` URI for the bundle's resource directory.
-   // The host may translate `file:` URIs to some other scheme or prefix, to limit access scope
-   // or handle virtual filesystems. Such pages must therefore not assume they has been loaded
-   // from any particular URI scheme, host or path prefix. The host must still preserve the
-   // filename and relative path to any resources within the same directory as that page.
+   // Relative URIs (with absolute paths) refer to resources provided by .get_resource(), which
+   // may be provided to the webview with an arbitrary scheme or URI prefix. The host may also
+   // translate `file:` URIs to some other scheme or path root, to limit access scope or handle
+   // virtual filesystems. Therefore, when using either relative or `file:` URIs, pages must not
+   // assume a particular absolute path, only relative paths between resources.
    // Returns the desired length of the URI, or <= 0 for an error. If this is greater than the
    // capacity, then the result was truncated. If the capacity is 0, `uri` may be a null pointer.
    // [main-thread]
-   int32_t (CLAP_ABI *get_uri)(const clap_plugin_t *plugin, char *uri, uint32_t uri_capacity);
+   int32_t(CLAP_ABI *get_uri)(const clap_plugin_t *plugin, char *uri, uint32_t uri_capacity);
+
+   // Provides the media type and data for resources, starting with the URI from get_uri().
+   // The path must be absolute (starting with `/`) with any host-defined path prefix removed.
+   // Returns true if the media type and entire contents of the resource were provided.
+   // [main-thread]
+   bool(CLAP_ABI *get_resource)(const clap_plugin_t *plugin, const char *path, char *mime, uint32_t mime_capacity, const clap_ostream_t *stream);
 
    // Receives a single message from the webview, which must be open and ready to receive replies.
    // Returns true on success.
