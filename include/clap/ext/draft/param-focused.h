905107accbc655e383d49c6600ec66c1fdb596df
David Schornsheim <6382400+Schroedingers-Cat@users.noreply.github.com>
2026-07-06 10:33:37 +0200

    Rename extension back to hovered

--- include/clap/ext/draft/param-focused.h
+++ include/clap/ext/draft/param-focused.h
@@ -2,27 +2,27 @@
 
 #include "../../plugin.h"
 
-// This extension lets a plugin report to the host the parameter's clap_id of its last focused UI
-// control and whether it is currently focused (usually hovered by pointer device).
+// This extension lets a plugin report to the host the parameter's clap_id of its last hovered UI
+// control and whether it is currently hovered (usually hovered by pointer device).
 
-static CLAP_CONSTEXPR const char CLAP_EXT_PARAM_FOCUSED[] = "clap.param-focused/1";
+static CLAP_CONSTEXPR const char CLAP_EXT_PARAM_HOVERED[] = "clap.param-hovered/1";
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct clap_host_param_focused {
-   // Plugin informs the host about a new focus state.
+typedef struct clap_host_param_hovered {
+   // Plugin informs the host about a new hover state.
    //
-   // Should be called whenever the focused UI control's parameter ID changes or when it changes
-   // from focused to not being focused.
+   // Should be called whenever the hovered UI control's parameter ID changes or when it changes
+   // from hovered to not being hovered.
    //
-   // Only one parameter can be focused at a time. Use CLAP_INVALID_ID as param_id if no parameter
-   // is focused.
+   // Only one parameter can be hovered at a time. Use CLAP_INVALID_ID as param_id if no parameter
+   // is hovered.
    //
    // [main-thread]
-   void(CLAP_ABI *update)(const clap_host_t *host, clap_id param_id);
-} clap_host_param_focused_t;
+   void(CLAP_ABI *update)(const clap_host_t *host, clap_id hovered_param_id);
+} clap_host_param_hovered_t;
 
 #ifdef __cplusplus
 }
